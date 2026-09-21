c72be3294ea772d7d21c382a6dcbffa551dfa54e
YUG JAIN <yug.jain.cseaiml.2024@miet.ac.in>
2026-09-21 19:17:39 +0530

    Time: 47 ms (38.55%) | Memory: 19.2 MB (26.15%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -5,10 +5,10 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         low,high = 1,n
         while(low<=high):
-            mid= low +(high-low)//2
+            mid= low+(high-low)//2
             if(isBadVersion(mid)):
-                high = mid-1
+                high=mid-1
             else:
-                low = mid+1
+                low= mid+1
         return low
-#whateverrrrrrrrrrrrr
+            
